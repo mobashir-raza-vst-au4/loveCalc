dbea341dbad98d0d12b7c50a3447b2969ef4458f
mobashir-raza-vst-au4 <54476228+mobashir-raza-vst-au4@users.noreply.github.com>
2020-06-02 22:44:35 +0530

    done

--- App.js
+++ App.js
@@ -1,19 +1,84 @@
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, Appbar, Button  } from 'react-native-paper';
+import React, { Component } from 'react'
+import Display from './Components/Display';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    text1: "",
+    text2: "",
+    data: "",
+    loading: "Loading...",
+    view: false,
+    status: false
+  }
+
+  handleSubmit = () => {
+    // console.log(Object.keys(this.state.data).length)
+    if(Object.keys(this.state.data).length > 0) {
+      this.setState({
+        data: "",
+        status: false
+      })
+    }
+    this.setState({
+      view: true
+    })
+    fetch(`https://love-calculator.p.rapidapi.com/getPercentage?fname=${this.state.text1}&sname=${this.state.text2}`,{
+      headers: {
+        "x-rapidapi-host": "love-calculator.p.rapidapi.com",
+        "x-rapidapi-key": "8abef6e1eemshbf043345aba09d6p132a31jsn2d0d6caf9ea4"
+      }
+    })
+    .then(data => data.json())
+    .then(data2 => {
+      // console.log(data2);
+      this.setState({
+        data: data2,
+        view: false,
+        status: true
+      })
+    })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Appbar.Header>
+          <Appbar.Content style={{alignItems:"center"}}
+            title="Love Calculator"
+          />
+        </Appbar.Header>
+
+        <TextInput
+        label='Male'
+        value={this.state.text1}
+        onChangeText={text1 => this.setState({ text1:text1 })}
+      />
+        <TextInput
+        label='Female'
+        value={this.state.text2}
+        onChangeText={text2 => this.setState({ text2:text2 })}
+      />
+
+      <Button style={{margin:10}} icon="heart" mode="contained" onPress={() => this.handleSubmit()}>
+          calculate
+      </Button>
+      {this.state.view && <Text style={styles.text}>{this.state.loading}</Text>}
+      <Display allData = {this.state.data} status = {this.state.status}/>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  text: {
+    textAlign:"center",
+    fontSize: 30,
+    marginTop:20
+}
+
 });
---
+++ Components/Display.js
@@ -0,0 +1,29 @@
+import React, { Component, Fragment } from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class display extends Component {
+    render() {
+        // console.log(typeof this.props);
+        return (
+            <View style={styles.container}>
+                {this.props.status && <Fragment>
+                <Text style={styles.text}>{this.props.allData.percentage}%</Text>
+                <Text style={styles.text}>{this.props.allData.result}</Text>
+                </Fragment>}
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+    text: {
+        textAlign:"center",
+        fontSize: 30,
+        marginTop:20
+    }
+  
+  });
